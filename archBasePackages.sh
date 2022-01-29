b20f5d22670b6229a5c7236a24facf369d669b1e
z.mauro.solidoro@guest.rcs.it <mauro.solidoro@guest.rcs.it>
2022-01-29 16:59:38 +0100

    add tools

--- archBasePackages.sh
+++ archBasePackages.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-# pipewire packages
+set -e
+
+# pipewire packages + webrtc
 sudo pacman -Sy lib32-pipewire lib32-pipewire-jack pipewire-jack libpipewire02 pipewire pipewire-alsa pipewire-pulse gst-plugin-pipewire
+sudo pacman -Sy xdg-desktop-portal xdg-desktop-portal-gnome xdg-desktop-portal-gtk --noconfirm
+
+# essential tools
+sudo pacman -Sy traceroute kubectl
+
+# hypervisor section
+sudo pacman -Sy vagrant virt-manager iptables-nft dnsmasq bridge-utils openbsd-netcat --noconfirm
 
 # optional software
+sudo pacman -Sy telegram-desktop  --noconfirm
 yay -Sy lens --noconfirm
