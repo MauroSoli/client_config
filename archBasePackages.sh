d6c620052949842507e26c141eb9bb225f523d53
z.mauro.solidoro@guest.rcs.it <mauro.solidoro@guest.rcs.it>
2022-01-29 16:18:30 +0100

    add lens

--- archBasePackages.sh
+++ archBasePackages.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # pipewire packages
 sudo pacman -Sy lib32-pipewire lib32-pipewire-jack pipewire-jack libpipewire02 pipewire pipewire-alsa pipewire-pulse gst-plugin-pipewire
+
+# optional software
+yay -Sy lens --noconfirm
