3b1c2933543cab3465e517056b26ee11740956fb
z.mauro.solidoro@guest.rcs.it <mauro.solidoro@guest.rcs.it>
2022-01-29 17:06:31 +0100

    bbho

--- archUpgrade.sh
+++ archUpgrade.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pacman -Sy archlinux-keyring --noconfirm
 pacman -Syu
-systemd-cryptenroll /dev/nvme0n1p1 --tpm2-pcrs=7+8 --tpm2-device=auto --wipe-slot=tpm2
+#systemd-cryptenroll /dev/nvme0n1p1 --tpm2-pcrs=7+8 --tpm2-device=auto --wipe-slot=tpm2
