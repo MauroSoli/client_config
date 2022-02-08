79e453561243b976761bffa83b22c551ceb97532
z.mauro.solidoro@guest.rcs.it <mauro.solidoro@guest.rcs.it>
2022-02-08 23:02:20 +0100

    add packages

--- archBasePackages.sh
+++ archBasePackages.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -e
 
+#Gpu and rendering
+sudo pacman -Sy xf86-video-nouveau --noconfirm
+
 # pipewire packages + webrtc
 sudo pacman -Sy lib32-pipewire lib32-pipewire-jack pipewire-jack libpipewire02 pipewire pipewire-alsa pipewire-pulse gst-plugin-pipewire
 sudo pacman -Sy xdg-desktop-portal xdg-desktop-portal-gnome xdg-desktop-portal-gtk --noconfirm
 
 # essential tools
 sudo pacman -Sy traceroute kubectl
+sudo pacman -Sy lshw --noconfirm
+sudo pacman -Sy jre-openjdk --noconfirm
 yay -Sy google-cloud-sdk --noconfirm
 
 # hypervisor section
