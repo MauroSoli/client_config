13193d20d80f1e795869e952b9e8d43804544d9d
z.mauro.solidoro@guest.rcs.it <mauro.solidoro@guest.rcs.it>
2022-02-15 11:12:33 +0100

    add podman

--- archBasePackages.sh
+++ archBasePackages.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 set -e
 
+# systemd-resolvd binary
+pacman -Sy systemd-resolvconf --noconfirm
+
 # GPU and rendering
-sudo pacman -Sy xf86-video-nouveau --noconfirm
+sudo pacman -S xf86-video-nouveau --noconfirm
 
 # Pipewire packages + webrtc
 sudo pacman -S lib32-pipewire lib32-pipewire-jack pipewire-jack libpipewire02 pipewire pipewire-alsa pipewire-pulse gst-plugin-pipewire
 sudo pacman -S xdg-desktop-portal xdg-desktop-portal-gnome xdg-desktop-portal-gtk --noconfirm
 
 # Essential tools
-sudo pacman -S traceroute kubectl
+sudo pacman -S kubectl
 sudo pacman -S lshw --noconfirm
 sudo pacman -S jre-openjdk --noconfirm
+sudo pacman -S podman --noconfirm
+sudo pacman -S traceroute
 yay -S google-cloud-sdk --noconfirm
 
 # Hypervisor section
-sudo pacman -S vagrant virt-manager iptables-nft dnsmasq bridge-utils openbsd-netcat --noconfirm
+sudo pacman -S vagrant virt-manager iptables-nft bridge-utils openbsd-netcat --noconfirm
 
 # Optional software
 sudo pacman -S telegram-desktop  --noconfirm
