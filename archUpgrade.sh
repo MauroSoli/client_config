9fb8f05ffbd4be8b543acb51c720b80c6b34165d
z.mauro.solidoro@guest.rcs.it <mauro.solidoro@guest.rcs.it>
2022-02-26 02:46:20 +0100

    add yay updates

--- archUpgrade.sh
+++ archUpgrade.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-pacman -Sy archlinux-keyring --noconfirm
-pacman -Syu
+sudo pacman -Sy archlinux-keyring --noconfirm
+sudo pacman -Syu
+yay -Syu
 #systemd-cryptenroll /dev/nvme0n1p1 --tpm2-pcrs=7+8 --tpm2-device=auto --wipe-slot=tpm2
