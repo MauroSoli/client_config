5b0e5d65c0610c9d251b1389d6edf224dbdd16ae
z.mauro.solidoro@guest.rcs.it <mauro.solidoro@guest.rcs.it>
2022-04-11 00:39:37 +0200

    add many fix

--- archBasePackages.sh
+++ archBasePackages.sh
@@ -1,26 +1,42 @@
 #!/bin/bash
 set -eu
 
+# Update system
+sudo pacman -Syu
+
+#Install git and yay
+sudo pacman -Sy git subversion --noconfirm --needed
+if [ ! -f "/bin/yay" ]; then
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+    cd ..
+    rm -rf yay
+fi
+
 # systemd-resolvd binary
-pacman -Sy systemd-resolvconf --noconfirm --needed
+sudo pacman -S systemd-resolvconf --noconfirm --needed
 
 # Pipewire packages + webrtc
-sudo pacman -S lib32-pipewire lib32-pipewire-jack pipewire-jack libpipewire02 pipewire pipewire-alsa pipewire-pulse gst-plugin-pipewire --needed
-sudo pacman -S xdg-desktop-portal xdg-desktop-portal-gnome xdg-desktop-portal-gtk --noconfirm --needed
+# lib32-pipewire lib32-pipewire-jack
+sudo pacman -S  pipewire-jack libpipewire02 pipewire pipewire-alsa pipewire-pulse gst-plugin-pipewire --needed
+sudo pacman -S  xdg-desktop-portal xdg-desktop-portal-gnome xdg-desktop-portal-gtk --noconfirm --needed
 
 # Gnome software
 sudo pacman -S gnome-shell  gnome-terminal  gnome-tweak-tool gnome-control-center xdg-user-dirs gdm --noconfirm --needed
-sudo pacman -S nautilus guake --noconfirm --needed
+sudo pacman -S nautilus --noconfirm --needed
 sudo pacman -S gnome-software gnome-software-packagekit-plugin gnome-multi-writer --noconfirm --needed
 sudo pacman -S networkmanager-fortisslvpn --noconfirm --needed
-pacman -S gnome-tweaks gnome-shell-extensions --noconfirm --needed
+sudo pacman -S gnome-tweaks gnome-shell-extensions --noconfirm --needed
 yay -S chrome-gnome-shell --noconfirm --needed
 
 # Essential tools/software
-sudo pacman -S git subversion --needed
-git lfs install
+sudo pacman -S git-lfs --noconfirm --needed
+git lfs install 
+sudo pacman -S vi net-tools --noconfirm --needed
 yay -Sy nautilus-compare --noconfirm --needed
-sudo pacman -S kubectl --needed
+sudo pacman -S terminator --noconfirm --needed
+sudo pacman -S kubectl --noconfirm --needed
 sudo pacman -S rclone --noconfirm --needed
 sudo pacman -S duplicity python-requests-oauthlib --noconfirm --needed 
 sudo pacman -S rsnapshot --noconfirm --needed
@@ -33,22 +49,28 @@ sudo pacman -S terragrunt --noconfirm --needed
 sudo pacman -S obs-studio --noconfirm --needed
 sudo pacman -S ansible --noconfirm --needed
 sudo pacman -S mariadb-clients --noconfirm --needed
+yay -S lens --removemake --noconfirm --needed # dev'essere prima di ATOM!
 yay -S google-chrome --noconfirm --needed
 yay -S openfortigui --noconfirm --needed
 yay -S tigervnc-viewer  --noconfirm --needed
+
+# Code editors
+sudo pacman -Sy code --noconfirm --needed
+yes | sudo pacman -S apm --noconfirm --needed
+sudo pacman -Sy atom --noconfirm --needed
 
 #Cloud CLI
 sudo pacman -S aws-cli --noconfirm --needed
 yay -S google-cloud-sdk --noconfirm --needed
 
 # Hypervisor section
-sudo pacman -S vagrant virt-manager iptables-nft bridge-utils openbsd-netcat --noconfirm --needed
+yes | sudo pacman -S iptables-nft --noconfirm --needed
+sudo pacman -S vagrant virt-manager bridge-utils openbsd-netcat --noconfirm --needed
 
 # Optional software
 sudo pacman -S telegram-desktop --noconfirm --needed
-yay -S lens --noconfirm --needed
-pacman -S solaar --noconfirm --needed
-pacman -S ipcalc --noconfirm --needed
+sudo pacman -S solaar --noconfirm --needed
+sudo pacman -S ipcalc --noconfirm --needed
 
 # Fonts
 sudo pacman -S ttf-cascadia-code --noconfirm --needed
