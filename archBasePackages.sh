745aa1e924cdbe31b42460e9d9aa8daac6319352
z.mauro.solidoro@guest.rcs.it <mauro.solidoro@guest.rcs.it>
2022-04-11 11:24:21 +0200

    fix remove dipendencies

--- archBasePackages.sh
+++ archBasePackages.sh
@@ -49,6 +49,7 @@ sudo pacman -S terragrunt --noconfirm --needed
 sudo pacman -S obs-studio --noconfirm --needed
 sudo pacman -S ansible --noconfirm --needed
 sudo pacman -S mariadb-clients --noconfirm --needed
+yes | sudo pacman -S nodejs-lts-fermium --needed
 sudo pacman -S nodejs-nopt libxss c-ares nodejs-lts-fermium nodejs-nopt semver node-gyp npm yarn --noconfirm --needed
 yay -S lens --removemake --noconfirm --needed # dev'essere prima di ATOM! Meglio non usare noconfirm
 yay -S google-chrome --noconfirm --needed
@@ -56,9 +57,9 @@ yay -S openfortigui --noconfirm --needed
 yay -S tigervnc-viewer  --noconfirm --needed
 
 # Code editors
-sudo pacman -Sy code --noconfirm --needed
-yes | sudo pacman -S apm --noconfirm --needed
-sudo pacman -Sy atom --noconfirm --needed
+sudo pacman -S code --noconfirm --needed
+yes | sudo pacman -S nodejs-lts-gallium --needed
+sudo pacman -S atom --noconfirm --needed
 
 #Cloud CLI
 sudo pacman -S aws-cli --noconfirm --needed
