eef56f8c8aaee22dbac967b3ff63e8f7de763086
z.mauro.solidoro@guest.rcs.it <mauro.solidoro@guest.rcs.it>
2022-04-16 21:36:21 +0200

    rsync

---
+++ rsync.sh
@@ -0,0 +1,40 @@
+systemctl enable systemd-resolved
+/home/linux/.icons \
+/home/linux/Downloads \
+
+sudo rsync -avzpg -R --progress \
+/home/linux/.atom \
+/home/linux/.bash* \
+/home/linux/.config \
+/home/linux/.duplicity* \
+/home/linux/.gitconfig \
+/home/linux/.k8slens \
+/home/linux/.kube \
+/home/linux/.local \
+/home/linux/.openfortigui \
+/home/linux/.ssh \
+/home/linux/.subversion \
+/home/linux/.vim* \
+/home/linux/.vnc \
+/home/linux/.vscode-oss \
+/home/linux/app \
+/home/linux/Documents \
+/home/linux/OneDrive \
+/home/linux/S3-RCS \
+/home/linux/Videos \
+/home/linux/Pictures \
+/etc/pacman.conf \
+/etc/systemd/journald.conf \
+/etc/ssh/ssh_config \
+/etc/UPower \
+/etc/rsnapshot.conf \
+/etc/hosts \
+/etc/resolv.conf \
+/etc/systemd/resolved.conf \
+/usr/share/icons \
+/etc/NetworkManager/system-connections/Vodafone-Valopolish.nmconnection \
+/etc/NetworkManager/system-connections/POLiSh\ CASA.nmconnection \
+/etc/NetworkManager/system-connections/RCS-VPN.nmconnection \
+/etc/NetworkManager/system-connections/Vodafone-Valopolish.nmconnection \
+/usr/share/gnome-shell \
+root@10.15.20.124:/
