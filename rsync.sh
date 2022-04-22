1933cc668da3d0f1f41549cecdaae992c9227f63
z.mauro.solidoro@guest.rcs.it <mauro.solidoro@guest.rcs.it>
2022-04-22 18:16:33 +0200

    add code

--- rsync.sh
+++ rsync.sh
@@ -1,9 +1,4 @@
-systemctl enable systemd-resolved
-/home/linux/.icons \
-/home/linux/Downloads \
-/etc/pacman.conf \
-
-
+# Rsync to PC migration
 sudo rsync -avzpg -R --progress \
 /home/linux/.atom \
 /home/linux/.bash* \
@@ -42,3 +37,48 @@ sudo rsync -avzpg -R --progress \
 /etc/NetworkManager/NetworkManager.conf
 /usr/share/gnome-shell \
 root@10.15.20.124:/
+
+
+
+# duplicity backup
+duplicity --name Dellarch --encrypt-key MauroSoli --full-if-older-than 3M --progress --verbosity info \
+--include="/home/linux/.atom" \
+--include="/home/linux/.bash*" \
+--include="/home/linux/.config" \
+--include="/home/linux/.duplicity*" \
+--include="/home/linux/.gitconfig" \
+--include="/home/linux/.k8slens" \
+--include="/home/linux/.kube" \
+--include="/home/linux/.local" \
+--include="/home/linux/.openfortigui" \
+--include="/home/linux/.ssh" \
+--include="/home/linux/.subversion" \
+--include="/home/linux/.vim*" \
+--include="/home/linux/.vnc" \
+--include="/home/linux/.vscode-oss" \
+--include="/home/linux/app" \
+--include="/home/linux/Documents" \
+--include="/home/linux/OneDrive" \
+--include="/home/linux/S3-RCS" \
+--include="/home/linux/Videos" \
+--include="/home/linux/Pictures" \
+--include="/etc/systemd/journald.conf" \
+--include="/etc/systemd/logind.conf" \
+--include="/etc/ssh/ssh_config" \
+--include="/etc/UPower" \
+--include="/etc/rsnapshot.conf" \
+--include="/etc/hosts" \
+--include="/etc/resolv.conf" \
+--include="/etc/systemd/resolved.conf" \
+--include="/usr/share/icons" \
+--include="/usr/share/themes" \
+--include="/etc/NetworkManager/system-connections/Vodafone-Valopolish.nmconnection" \
+--include="/etc/NetworkManager/system-connections/POLiSh\ CASA.nmconnection" \
+--include="/etc/NetworkManager/system-connections/RCS-VPN.nmconnection" \
+--include="/etc/NetworkManager/system-connections/Vodafone-Valopolish.nmconnection" \
+--include="/etc/NetworkManager/NetworkManager.conf" \
+--include="/usr/share/gnome-shell" \
+--exclude='**' \
+/ \
+onedrive://duplicity/
+
