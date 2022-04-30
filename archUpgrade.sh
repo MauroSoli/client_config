dbc01151ed9bd1da757d5247a41fd2f81e2ef966
z.mauro.solidoro@guest.rcs.it <mauro.solidoro@guest.rcs.it>
2022-04-30 13:15:41 +0200

    fix upgrade order

--- archUpgrade.sh
+++ archUpgrade.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo pacman -Sy archlinux-keyring --noconfirm
-sudo pacman -R nodejs-lts-gallium apm atom npm node-gyp nodejs-nopt npm semver yarn --noconfirm
+sudo pacman -R nodejs-lts-gallium apm atom --noconfirm
 sudo pacman -Syu
-yay -Syu
+yay -Syu --removemake
 yes | sudo pacman -S nodejs-lts-gallium --needed
 sudo pacman -S atom --noconfirm --needed
 #systemd-cryptenroll /dev/nvme0n1p1 --tpm2-pcrs=7+8 --tpm2-device=auto --wipe-slot=tpm2
