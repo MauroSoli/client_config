2c908b3ac5c7237416df6d8437b9daafb7549070
z.mauro.solidoro@guest.rcs.it <mauro.solidoro@guest.rcs.it>
2022-06-15 10:55:33 +0200

    add ApacheConfig

--- archRCSConfig.sh
+++ archRCSConfig.sh
@@ -2,23 +2,23 @@
 # Git configuration
 mkdir -p ~/git/
 if [ $? = "0" ]; then
-    git     clone git@gitlab-internal.rcslan.it:CDT/utilities.git               ~/git/utilities
-    git     clone git@gitlab-internal.rcslan.it:CDT/aws-terraform.git           ~/git/aws-terraform
-    git lfs clone git@gitlab-internal.rcslan.it:CDT/terraform.git               ~/git/terraform 
-    git     clone git@gitlab-internal.rcslan.it:CDT/solrcloud-config.git        ~/git/solrcloud-config
-    git     clone git@gitlab-internal.rcslan.it:CDT/Jenkinsfiles.git            ~/git/Jenkinsfiles
-    git     clone git@gitlab-internal.rcslan.it:CDT/varnish-configuration.git   ~/git/varnish-configuration
+    git     clone git@gitlab-internal.rcslan.it:CDT/utilities.git                                       ~/git/utilities
+    git     clone git@gitlab-internal.rcslan.it:CDT/aws-terraform.git                                   ~/git/aws-terraform
+    git lfs clone git@gitlab-internal.rcslan.it:CDT/terraform.git                                       ~/git/terraform 
+    git     clone git@gitlab-internal.rcslan.it:CDT/solrcloud-config.git                                ~/git/solrcloud-config
+    git     clone git@gitlab-internal.rcslan.it:CDT/Jenkinsfiles.git                                    ~/git/Jenkinsfiles
+    git     clone git@gitlab-internal.rcslan.it:CDT/varnish-configuration.git                           ~/git/varnish-configuration
+    git     clone git@gitlab-internal.rcslan.it:CDT/httpd-configuration.git                             ~/git/httpd-configuration
     git     clone git@gitlab-internal.rcslan.it:CDT/fastly-configurations/wfprcor.rcs.it.git            ~/git/fastly-configurations/wfprcor.rcs.it
     git     clone git@gitlab-internal.rcslan.it:CDT/fastly-configurations/meteo.corriere.it.git         ~/git/fastly-configurations/meteo.corriere.it
     git     clone git@gitlab-internal.rcslan.it:CDT/fastly-configurations/wfdvgaz-xalok.git             ~/git/fastly-configurations/wfdvgaz-xalok
     git     clone git@gitlab-internal.rcslan.it:CDT/fastly-configurations/wfdvgaz.git                   ~/git/fastly-configurations/wfdvgaz
     git     clone git@gitlab-internal.rcslan.it:CDT/fastly-configurations/prgaz.rcs.it.git              ~/git/fastly-configurations/prgaz.rcs.it
     git     clone git@gitlab-internal.rcslan.it:CDT/fastly-configurations/static-wfprcor.rcs.it.git     ~/git/fastly-configurations/static-wfprcor.rcs.it
-    git     clone git@gitlab-internal.rcslan.it:CDT/xalok/image-building.git    ~/git/xalok/image-building
-    git     clone git@gitlab-internal.rcslan.it:CDT/xalok/rcs-corriere.git      ~/git/xalok/rcs-corriere
-    git     clone git@gitlab-internal.rcslan.it:CDT/xalok/rcs-gazzetta.git      ~/git/xalok/rcs-gazzetta
-    git     clone git@gitlab-internal.rcslan.it:CDT/xalok/rcs-provision.git     ~/git/xalok/rcs-provision
-    cd ..
+#    git     clone git@gitlab-internal.rcslan.it:CDT/xalok/image-building.git                            ~/git/xalok/image-building
+#    git     clone git@gitlab-internal.rcslan.it:CDT/xalok/rcs-corriere.git                              ~/git/xalok/rcs-corriere
+#    git     clone git@gitlab-internal.rcslan.it:CDT/xalok/rcs-gazzetta.git                              ~/git/xalok/rcs-gazzetta
+#    git     clone git@gitlab-internal.rcslan.it:CDT/xalok/rcs-provision.git                             ~/git/xalok/rcs-provision
 else
  echo "Problem with folder creation"
 fi
