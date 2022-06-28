5d032bbfd69c60389429cb298b2c05d556c75166
z.mauro.solidoro@guest.rcs.it <mauro.solidoro@guest.rcs.it>
2022-06-28 19:57:27 +0200

    add fzf and ripgrep-all

--- archBasePackages.sh
+++ archBasePackages.sh
@@ -62,6 +62,7 @@ sudo pacman -S python-pip --noconfirm --needed
 sudo pip install pre-commit
 sudo pacman -S vi pwgen man net-tools unrar unzip xz wget inetutils --noconfirm --needed
 sudo pacman -S atop htop --noconfirm --needed
+sudo pacman -S fzf ripgrep-all --noconfirm --needed
 sudo pacman -S nfs-utils nilfs-utils ntp smartmontools strace nmap tcpdump wireshark-qt --noconfirm --needed
 sudo pacman -S ncdu meld --noconfirm --needed
 sudo pacman -S terminator --noconfirm --needed
